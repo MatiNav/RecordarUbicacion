90da53186f9cb740c535ac9e24355d43cc54bede
Huenei desarrollo <desarrollo@hci-mac-05.local>
2017-02-13 10:19:54 -0300

    Initial Commit

---
+++ Recordar_Ubicacion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Recordar_Ubicacion
+//
+//  Created by Huenei desarrollo on 13/2/17.
+//  Copyright © 2017 Huenei desarrollo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Recordar_Ubicacion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Recordar_Ubicacion
+//
+//  Created by Huenei desarrollo on 13/2/17.
+//  Copyright © 2017 Huenei desarrollo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
